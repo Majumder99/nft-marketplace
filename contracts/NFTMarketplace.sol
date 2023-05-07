5949849e889a856ef5f76ec4663fbe84eaa76e4b
Sourav Majumder <souravmajumderk99@gmail.com>
2023-05-07 23:50:20 +0600

    contract update

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -10,7 +10,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract NFTMarketplace is ERC721URIStorage{
     address payable owner;
-
+    uint256 public totalSupply = 1000;
     using Counters for Counters.Counter;
     Counters.Counter private _tokenId;
     Counters.Counter private _itemsSold;
@@ -28,5 +28,52 @@ contract NFTMarketplace is ERC721URIStorage{
         uint256 price;
         bool currentlyListed;
     }
-    
+
+    mapping(uint256 => ListsToken) private listedTokens;
+
+    function updateListPrice(uint256 _listPrice) public payable{
+        require(owner == msg.sender, "Only owner can update the listing price");
+        listPrice = _listPrice;
+    }
+
+    function getListPrice() public view returns(uint256){
+        return listPrice;
+    }
+
+    function getLatestIdListedToken() public view returns(ListsToken memory){
+        uint256 currentToken = _tokenId.current();
+        return listedTokens[currentToken];
+    }
+
+    function getListedToken(uint256 tokenId) public view returns(ListsToken memory){
+        return listedTokens[tokenId];
+    }
+
+    function getCurrentToken() public view returns(uint256){
+        return _tokenId.current();
+    }
+
+    function createToken(string memory tokenURI, uint256 price) public payable returns (uint){
+        _tokenId.increment();
+        uint256 currentToken = _tokenId.current();
+        require(price >= listPrice, "Price should be equal or larget than 0.05");
+        require(currentToken <= totalSupply, "Total supply is extended");
+
+        _safeMint(msg.sender, currentToken);
+        _setTokenURI(currentToken, tokenURI);
+        setListedToken(currentToken, price);
+        return currentToken;
+    }
+
+    function setListedToken(uint256 tokenId, uint256 price) public {
+        listedTokens[tokenId] = ListsToken(
+            tokenId, 
+            payable(address(this)),
+            payable(msg.sender), 
+            price,
+            true
+        );
+        // I am sending my token to the contract address 
+        _transfer(msg.sender, address(this), tokenId);
+    }
 }
